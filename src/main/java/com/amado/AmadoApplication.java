246b2f3033d53ec8acc24b909340204c600f200c
bhupendrasam1404 <bhupendrasam1404@gmail.com>
2022-11-28 18:58:50 +0530

    tomcat config done

--- src/main/java/com/amado/AmadoApplication.java
+++ src/main/java/com/amado/AmadoApplication.java
@@ -2,9 +2,10 @@ package com.amado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AmadoApplication {
+public class AmadoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmadoApplication.class, args);
